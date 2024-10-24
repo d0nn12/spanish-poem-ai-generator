0aa26cab1f4712a77a5aaad4b06303ddd85e08bb
d0nn12 <pilarstrasburg@gmail.com>
2024-10-24 14:56:25 -0400

    api integration and instructions

--- javascript/java.js
+++ javascript/java.js
@@ -1,12 +1,22 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: ["La pizza es mi mejor amiga"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3bace0c34doe40bafa090t390947e976";
+  let context = `Generate a 6 line poem about ${instructionsInput.value}. Keep it simple, short and appropriate. Break every line to present as a poem.`;
+  let prompt = "Write a poem in spanish about the topic submitted";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
