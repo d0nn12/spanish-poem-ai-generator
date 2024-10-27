9bbcd421d2f935eebd33410b2a35a94158b875e6
d0nn12 <pilarstrasburg@gmail.com>
2024-10-27 13:47:33 -0400

    i don't know

--- javascript/java.js
+++ javascript/java.js
@@ -12,7 +12,7 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "3bace0c34doe40bafa090t390947e976";
-  let context = `Generate a 6 line poem about ${instructionsInput.value}. Keep it simple, short and appropriate. Break every line to present as a poem.`;
+  let context = `Generate a 6 line poem about ${instructionsInput.value}. Keep it simple, short and appropriate. Please, break after every "," to present as a poem.`;
   let prompt = "Write a poem in spanish about the topic submitted";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
@@ -20,3 +20,7 @@ function generatePoem(event) {
 }
 let poemFormElement = document.querySelector("#poem-generator-form");
 poemFormElement.addEventListener("submit", generatePoem);
+
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = "⏳Generating Spanish poem...";
